45768df687e4012acdd9a3f6da0f8e8c94b9486b
Matěj <vas@email.com>
2018-09-24 20:01:32 +0200

    AAAAAAAaaaaaaaaaaaaaaa

---
+++ js/config.js
@@ -0,0 +1,126 @@
+﻿/*    _           _    _                     _
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "ArchBox";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = true;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/colorfulburst.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/Alice.jpg",
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{youtube: "TKfS5zVfGBc", name: "Trance - 009 Sound System Dreamscape"},
+	{youtube: "c_H3MWVx6JU", name: "009 Sound System OFFICIAL"},
+	{youtube: "xi20Si3jRIo", name: "N.O.H.A. - Tu Cafe´"},
+	{youtube: "Be3xAAtPEdg", name: "Party Maker"},
+	{youtube: "Ly7uj0JwgKg", name: "Flume - Never be like you"},
+	{youtube: "xMP-JqFQ_l4", name: "Vengaboys - Boom, Boom, Boom, Boom!!"},
+	{youtube: "nn5hCEMyE-E", name: "DJ Bobo - THERE IS A PARTY"},
+	{youtube: "IRvGZffXhfk", name: "ATC - All Around The World (la la la la la la la la)"},
+	{youtube: "mcf79OWnitM", name: "Pakito - Living On Video"},
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 10;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Welcome to ArchBox!",
+"To open menu type : !menu",
+"Make sure to read the MOTD to know the rules!",
+"Like this server? Join our steam group!",
+"Enjoy your stay at ArchBox server!♥",
+"Remember to be kind and follow rules!",
+"You can find addon list at !motd",
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
